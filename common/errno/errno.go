e05e22147b9b3710f31b56412b93b19f3f7582ad
孙瑞 <smallrui@foxmail.com>
2021-12-31 23:56:17 +0800

    1、加入 go.mod go.sum 2、加入 error 404 405 handle

--- common/errno/errno.go
+++ common/errno/errno.go
@@ -4,30 +4,30 @@ import (
 	"encoding/json"
 )
 
-type IErrorCode interface {
-	WithId(id string) *ErrorCode
-	WithData(data interface{}) *ErrorCode
+type IErrNo interface {
+	WithId(id string) *ErrNo
+	WithData(data interface{}) *ErrNo
 }
 
-type ErrorCode struct {
+type ErrNo struct {
 	Code    string      `json:"code"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
 }
 
-func (code *ErrorCode) WithKeyPair(key string, value string) *ErrorCode {
+func (code *ErrNo) WithKeyPair(key string, value string) *ErrNo {
 	dataMap := make(map[string]string)
 	dataMap[key] = value
 	code.Data = dataMap
 	return code
 }
 
-func (code *ErrorCode) WithData(data interface{}) *ErrorCode {
+func (code *ErrNo) WithData(data interface{}) *ErrNo {
 	code.Data = data
 	return code
 }
 
-func (code *ErrorCode) String() string {
+func (code *ErrNo) String() string {
 	marshal, err := json.Marshal(code)
 	if err != nil {
 		return ""
@@ -36,24 +36,24 @@ func (code *ErrorCode) String() string {
 	return string(marshal)
 }
 
-func MakeErrorCode(name string, message string) ErrorCode {
-	return ErrorCode{
+func MakeErrNo(name string, message string) ErrNo {
+	return ErrNo{
 		Code:    name,
 		Message: message,
 	}
 }
 
 var (
-	Ok               = MakeErrorCode("Ok", "成功")
-	NoAuth           = MakeErrorCode("NoAuth", "没有登录")
-	Forbidden        = MakeErrorCode("Forbidden", "没有权限")
-	NotFound         = MakeErrorCode("NotFound", "不存在")
-	Duplicate        = MakeErrorCode("Duplicate", "已经存在")
-	ExceedLimit      = MakeErrorCode("ExceedLimit", "超出限制")
-	IllegalData      = MakeErrorCode("IllegalData", "数据不合法")
-	RepeatSubmit     = MakeErrorCode("RepeatSubmit", "重复提交")
-	ParameterError   = MakeErrorCode("ParameterError", "参数错误")
-	MethodNotAllowed = MakeErrorCode("MethodNotAllowed", "请求方式允许")
-	InternalError    = MakeErrorCode("InternalError", "内部错误")
-	ThirdPartError   = MakeErrorCode("ThirdPartError", "第三方错误")
+	Ok               = MakeErrNo("Ok", "成功")
+	NoAuth           = MakeErrNo("NoAuth", "没有登录")
+	Forbidden        = MakeErrNo("Forbidden", "没有权限")
+	NotFound         = MakeErrNo("NotFound", "不存在")
+	Duplicate        = MakeErrNo("Duplicate", "已经存在")
+	ExceedLimit      = MakeErrNo("ExceedLimit", "超出限制")
+	IllegalData      = MakeErrNo("IllegalData", "数据不合法")
+	RepeatSubmit     = MakeErrNo("RepeatSubmit", "重复提交")
+	ParameterError   = MakeErrNo("ParameterError", "参数错误")
+	MethodNotAllowed = MakeErrNo("MethodNotAllowed", "请求方式允许")
+	InternalError    = MakeErrNo("InternalError", "内部错误")
+	ThirdPartError   = MakeErrNo("ThirdPartError", "第三方错误")
 )
