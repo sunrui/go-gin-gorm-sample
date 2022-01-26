fb35ab6820409622377fe41180a14d910666c099
sunrui <smallrui@foxmail.com>
2022-01-26 16:49:30 +0800

    1、将 code 通用码放至在 result 中。 2、将 enum 相关放至在 enum.go 中。

--- framework/result/result.go
+++ framework/result/result.go
@@ -45,9 +45,25 @@ func (result Result) String() string {
 }
 
 // 创建结果对象
-func CreateResult(code string, message string) Result {
+func createResult(code string, message string) Result {
 	return Result{
 		Code:    code,
 		Message: message,
 	}
 }
+
+// 通用返回对象码
+var (
+	Ok               = createResult("Ok", "成功")
+	NoAuth           = createResult("NoAuth", "没有登录")
+	Duplicate        = createResult("Duplicate", "已经存在")
+	Forbidden        = createResult("Forbidden", "没有权限")
+	NotFound         = createResult("NotFound", "不存在")
+	NotMatch         = createResult("NotMatch", "不匹配")
+	RateLimit        = createResult("ExceedLimit", "超出限制")
+	LogicError       = createResult("LogicError", "逻辑错误")
+	ParameterError   = createResult("ParameterError", "参数错误")
+	MethodNotAllowed = createResult("MethodNotAllowed", "请求方式不允许")
+	InternalError    = createResult("InternalError", "内部错误")
+	ThirdPartError   = createResult("ThirdPartError", "第三方错误")
+)
